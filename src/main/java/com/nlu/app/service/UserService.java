72cee766e282f242e6cb6e0671e214780ce86296
lehiyen <74060757+lehiyen@users.noreply.github.com>
2022-07-02 10:20:16 +0700

    login

--- src/main/java/com/nlu/app/service/UserService.java
+++ src/main/java/com/nlu/app/service/UserService.java
@@ -1,5 +1,5 @@
 package com.nlu.app.service;
 
 public interface UserService {
-
+	public boolean checkLogin(String username, String password);
 }
--- src/main/java/com/nlu/app/service/UserServiceImpl.java
+++ src/main/java/com/nlu/app/service/UserServiceImpl.java
@@ -1,5 +1,25 @@
 package com.nlu.app.service;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nlu.app.entity.UserEntity;
+import com.nlu.app.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public boolean checkLogin(String username, String password) {
+		for (UserEntity user : userRepository.findAll()) {
+			if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 
 }
